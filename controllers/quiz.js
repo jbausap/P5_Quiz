b2c5955817145d44e67ba2d30154b58362643f13
Juan Adolfo Bausa Pérez <ja.bausa@alumnos.upm.es>
2018-05-04 15:23:52 +0200

    wer

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,10 +1,12 @@
 const {models} = require("../models");
 
 exports.index = ( req, res, next) => {
-    models.quizzes.findAll()
-        .then ( quizzes => {
-            res.render("quizzes.js", {quizzes})
-
-        })
-        .catch( error => next (error));
-}
+    models.quiz.findAll()
+    .then ( quizzes => {
+        res.render("quizzes", {quizzes})
+    })
+    .catch( error => {
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",error);
+        next(error);
+    });
+};
